cb5ffcd1bdcc27b8787bc67ddf648b97c8a0947a
furqanullah <47421157+furqanullah@users.noreply.github.com>
2023-08-20 19:36:34 +0530

    push

--- src/main/java/com/furru/HolidayBooking.java
+++ src/main/java/com/furru/HolidayBooking.java
@@ -3,7 +3,7 @@ package com.furru;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
-
+//edit
 @SpringBootApplication
 @PropertySource(value = { "classpath:messages.properties" })
 public class HolidayBooking {
